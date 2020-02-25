783d1a9f1709d78a49f5789653bf58763ded546c
Enyarina <57133245+Enyarina@users.noreply.github.com>
2020-02-25 17:15:40 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -28,14 +28,14 @@ TEST(Stack, push_test_2)
 
 TEST(Stack, push_test_3)
 {
-    Stack<int> my_stack{};
+    Stack<char> my_stack{};
 
-    my_stack.push(1);
-    EXPECT_EQ(my_stack.Head(), 1);
-    my_stack.push(5);
-    EXPECT_EQ(my_stack.Head(), 5);
-    my_stack.push(10);
-    EXPECT_EQ(my_stack.Head(), 10);
+    my_stack.push('1');
+    EXPECT_EQ(my_stack.Head(), '1');
+    my_stack.push('5');
+    EXPECT_EQ(my_stack.Head(), '5');
+    my_stack.push('10');
+    EXPECT_EQ(my_stack.Head(), '10');
 }
 
 TEST(Stack, pop_test_1)
