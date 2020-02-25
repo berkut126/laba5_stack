421e2f364cc24f0329788464ad918515f77e3faf
Enyarina <57133245+Enyarina@users.noreply.github.com>
2020-02-25 18:01:45 +0300

    Update test.cpp
    Added both char and int tests.

--- tests/test.cpp
+++ tests/test.cpp
@@ -18,56 +18,82 @@ EXPECT_EQ(std::is_move_assignable<Stack<std::string>>::value, true);
 TEST(Stack, push_test_2)
 {
     Stack<int> my_stack{};
-
     my_stack.push(1);
     my_stack.push(5);
     my_stack.push(10);
-
     EXPECT_EQ(my_stack.Head(), 10);
+    
+    Stack<char> my_stack2{};
+    my_stack2.push('1');
+    my_stack2.push('5');
+    my_stack2.push('8');
+    EXPECT_EQ(my_stack2.Head(), '8');
 }
 
 TEST(Stack, push_test_3)
 {
-    Stack<char, int> my_stack{};
-
+    Stack<char> my_stack{};
     my_stack.push('1');
     EXPECT_EQ(my_stack.Head(), '1');
-    my_stack.push(5);
-    EXPECT_EQ(my_stack.Head(), 5);
+    my_stack.push('5');
+    EXPECT_EQ(my_stack.Head(), '5');
     my_stack.push('8');
     EXPECT_EQ(my_stack.Head(), '8');
+    
+    Stack<int> my_stack2();
+    my_stack2.push(1);
+    EXPECT_EQ(my_stack2.Head(), 1);
+    my_stack2.push(5);
+    EXPECT_EQ(my_stack2.Head(), 5);
+    my_stack2.push(8);
+    EXPECT_EQ(my_stack2.Head(), 8);
 }
 
 TEST(Stack, pop_test_1)
 {
     F_Stack<int> my_stack{};
-
     my_stack.push(1);
     my_stack.push(5);
     my_stack.pop();
-
     EXPECT_EQ(my_stack.Head(), 1);
+    
+    F_Stack<char> my_stack2{};
+    my_stack2.push('1');
+    my_stack2.push('5');
+    my_stack2.pop();
+    EXPECT_EQ(my_stack2.Head(), '1');
 }
 
 TEST(Stack, push_emplace)
 {
     S_Stack<int> my_stack{};
-
     my_stack.push_emplace(1);
     EXPECT_EQ(my_stack.Head(), 1);
     my_stack.push_emplace(5);
     EXPECT_EQ(my_stack.Head(), 5);
     my_stack.push_emplace(10);
     EXPECT_EQ(my_stack.Head(), 10);
+    
+    S_Stack<char> my_stack2{};
+    my_stack2.push_emplace('1');
+    EXPECT_EQ(my_stack2.Head(), '1');
+    my_stack2.push_emplace('5');
+    EXPECT_EQ(my_stack2.Head(), '5');
+    my_stack2.push_emplace('8');
+    EXPECT_EQ(my_stack2.Head(), '8');
 }
 
 TEST(Stack, pop_test)
 {
     S_Stack<int> my_stack{};
-
     my_stack.push(1);
     my_stack.push(5);
     my_stack.pop();
-
     EXPECT_EQ(my_stack.Head(), 1);
+    
+    S_Stack<char> my_stack2{};
+    my_stack2.push('1');
+    my_stack2.push('5');
+    my_stack2.pop();
+    EXPECT_EQ(my_stack2.Head(), '1');
 }
